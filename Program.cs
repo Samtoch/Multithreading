4bb3a142306e7e25592f61d21cd9a15bb5b13c0a
Samuel Offiah <samtochi@outlook.com>
2022-11-03 15:50:09 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Multithreading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(DisplayOne);
+            Thread thread2 = new Thread(DisplayTwo);
+            thread1.Start();
+            thread2.Start();
+            Console.ReadLine();
+        }
+
+        public static void DisplayOne()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                Console.Write("Runing First Thread: " + i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void DisplayTwo()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Runing Second Thread: " + i);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
